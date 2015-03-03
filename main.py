8f51ec1b5d85bf2b67d60aeb7aed45bc587f2fa5
Benjamin Meinl <b.meinl@gmail.com>
2015-03-03 11:51:12 +0100

    Added some twitter functionality, does regular stream updates now.

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
 import praw
 import urllib2 as urllib
+from urllib import urlencode
 import json
 from time import strftime
 import arrow
+from PIL import Image
+import io
+import re
+import twitter
+import sys
 
 
 def getTopStreams(game, count=7):
-    from urllib import urlencode
     get_params = urlencode({'game': game})
     req = urllib.Request('https://api.twitch.tv/kraken/streams?' + get_params,
                          headers={'Accept': 'application/vnd.twitchtv.v3+json'})
     res = urllib.urlopen(req)
     streams = json.loads(res.read())['streams'][:count]
-    return [ { 'name': s['channel']['name'],
-               'viewers': s['viewers'],
-               'preview': s['preview']['template'],
-               'url': s['channel']['url'],
-               'status': s['channel']['status'],
-             } for s in streams
-           ]
+    try:
+        return [ { 'name': s['channel']['name'],
+                   'viewers': s['viewers'],
+                   'preview': s['preview']['template'],
+                   'url': s['channel']['url'],
+                   'status': s['channel']['status'],
+               } for s in streams
+        ]
+    except KeyError as e:
+        print streams
+        return []
 
 
 def makeSpritesheet(urls, width, height):
-    from PIL import Image
-    import io
-
     spritesheet = Image.new('RGB', (len(urls) * width, height))
 
     for i, url in enumerate(urls):
@@ -42,12 +48,29 @@ def streams_to_markdown(streams):
            u">>[](//)\n"
            u"\n").format(i=i, **stream)
           for (i, stream) in enumerate(streams)
-      ]
+    ]
     return ''.join(md)
 
 
-def main(subreddit='streetfightercss'):
-    import re
+def login_twitter(configfile):
+    with open(configfile) as f:
+        config = [line.strip('\r\n') for line in f]
+    api = twitter.Api(consumer_key=config[0], 
+                      consumer_secret=config[1],
+                      access_token_key=config[2],
+                      access_token_secret=config[3])
+    return api
+
+
+def get_good_tweets(api, count=7):
+    users = [str(user.id) for user in api.GetListMembers(None, slug='fgc', owner_screen_name='soulsynapse')]
+    keywords = ['footsies', 'ultra', 'kappa', 'hype', 'ull', 'lol']
+    tweets = api.GetStreamFilter(follow=users)
+    return tweets
+
+
+def update_sidebar(subreddit='streetfightercss'):
+    print "{}: Starting to update sidebar, try not to interrupt.".format(arrow.now().isoformat())
     r = praw.Reddit(user_agent='crossplatform:sidebot:v0.1 (by /u/SweetScientist)')
     r.login()
     settings = r.get_settings(subreddit)
@@ -57,19 +80,23 @@ def main(subreddit='streetfightercss'):
     pat = r"(?<={}).*?(?={})".format(re.escape("[**Live Streams**](http://reddit.com/#heading)\n\n\n"),
                                      re.escape("[**SFxTwitter**](http://reddit.com/#heading)"))
     streams = getTopStreams('Ultra Street Fighter IV')
-    print len(streams)
     stream_md = streams_to_markdown(streams)
     updated_sidebar = re.sub(pat, stream_md, sidebar, flags=re.DOTALL|re.UNICODE)
     makeSpritesheet([s['preview'] for s in streams], 45, 30)    
     r.upload_image(sub, 'twitchimages.jpg')
     try:
         # this raises captcha exception but it still works, *shrug*
-        r.update_settings(sub, description=updated_sidebar)
+        r.update_settings(sub, description=updated_sidebar, hide_ads=None)
     except praw.errors.InvalidCaptcha as e:
         pass
     # have to do this or it won't show new spritesheet
     r.set_stylesheet(sub, stylesheet)
-    print "{}: Updated sidebar.".format(arrow.now().isoformat())
+    print "{}: Updated sidebar.\n\n".format(arrow.now().isoformat())
+
 
 if __name__ == '__main__':
-    main()
+    import time
+    while True:
+        update_sidebar('streetfightercss')
+        time.sleep(60)
+
